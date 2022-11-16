a3fc7337b66095829f5fe821f7c945c30759451a
Camilo <churtadoh92@gmail.com>
2022-11-16 15:20:00 -0500

    update

--- api/src/config.js
+++ api/src/config.js
@@ -7,4 +7,5 @@ const DB_HOST=process.env.DB_HOST
 const DB_NAME= process.env.DB_NAME || 'juira'
 const DB_PORT= process.env.DB_PORT || 5432
 const GOOGLE_CREDENTIALS = process.env.GOOGLE_CREDENTIALS
-module.exports={PORT, DB_USER,DB_PASSWORD,DB_HOST, DB_NAME,DB_PORT, GOOGLE_CREDENTIALS} 
+const DB_DEPLOY = process.env.DB_DEPLOY
+module.exports={PORT, DB_USER,DB_PASSWORD,DB_HOST, DB_NAME,DB_PORT, GOOGLE_CREDENTIALS, DB_DEPLOY} 
--- api/src/db.js
+++ api/src/db.js
@@ -3,12 +3,17 @@ const {Sequelize} = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const { userInfo } = require('os');
-const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST,DB_PORT } = require('./config');
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST,DB_PORT, DB_DEPLOY } = require('./config');
 
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+const sequelize = new Sequelize(DB_DEPLOY, {
     logging: false,
     native: false,
+    dialectOptions : {
+      ssl: {
+        require: true
+      }
+    },
     port:DB_PORT
 });
 const basename = path.basename(__filename);
